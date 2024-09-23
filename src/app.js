df689f96fde21db5ada30becc54a998896d55005
sanskritiG21 <21sanskritigupta@gmail.com>
2024-09-24 02:00:06 +0530

    error handling bhi seekh li

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
 // starting point of the application
-
 const express = require("express");
 
 const app = express(); // instance of this express JS application
 
-app.use("/", (req, res) => {
-  res.send("Dashboard");
+app.get("/getUserData", (req, res, next) => {
+  try {
+    throw new Error("bhai code fat gaya");
+  } catch (err) {
+    res.status(500).send("error handle kardunga pura pura");
+  }
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello world");
-});
-
-app.use("/test", (req, res) => {
-  // request handler function
-  res.send("hello from the server");
+app.use("/", (err, req, res, next) => {
+  // handle error gracefully
+  if (err) {
+    console.log(err);
+    res.status(500).send("lol yaha send krna tha error");
+  }
 });
 
 const PORT = 3030;
