1452616057580a7552256dfa34f07efa87057a05
sanskritiG21 <21sanskritigupta@gmail.com>
2024-10-07 02:28:46 +0530

    added connectionRequest api and model

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: "{VALUE} is not valid status",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+connectionRequestSchema.pre("save", function (next) {
+  const connectionReq = this;
+
+  if (connectionReq.toUserId.equals(connectionReq.fromUserId)) {
+    throw new Error("Can not send request to yourself");
+  }
+  next();
+});
+
+const ConnectionRequest = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
+
+module.exports = ConnectionRequest;
--- src/routes/requestRoute.js
+++ src/routes/requestRoute.js
@@ -1,15 +1,66 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const requestRoute = express.Router();
 
-requestRoute.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-    res.send(`${user.firstName} sent a connection request`);
-  } catch (err) {
-    res.status(400).send({ message: err.message });
+// Post - /request/send/:status/:userId
+requestRoute.post(
+  "/request/send/:status/:receiverId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user.id;
+      const toUserId = req.params.receiverId;
+      const status = req.params.status;
+
+      const allowedStatus = ["ignored", "interested"];
+
+      // case 1: status should be only ignored or interested;
+      if (!allowedStatus.includes(status)) {
+        throw new Error("Invalid Status");
+      }
+
+      // case 2: user should be only able to send request to reciver once, and if there is already a request from  the users side then he should not be able to send request;
+      const existingRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingRequest) {
+        throw new Error("Connection already exists");
+      }
+
+      // case 3: receiverID should not be able random, It should be a valid reciever, check from DB
+      const validReceiver = await User.findById(toUserId);
+
+      if (!validReceiver) {
+        return res.status(404).json({ message: "User doesn't exist" });
+      }
+
+      // case 4: user should not be able to send req to himself - handled in connectionRequestModel
+
+      // make the request;
+      const data = await ConnectionRequest.create({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      return res.json({
+        message:
+          status === "ignored"
+            ? `${req.user.firstName} disliked ${validReceiver.firstName}`
+            : `${req.user.firstName} sent request to ${validReceiver.firstName}`,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send({ message: err.message });
+    }
   }
-});
+);
 
 module.exports = requestRoute;
